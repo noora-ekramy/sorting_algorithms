e3fcdfa1966c08e0930d2019a79c5231f7f31b72
Nora <nora.ekramy11@gmail.com>
2023-11-18 12:39:58 +0200

    betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,14 +7,16 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current, *tmp;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *current = (*list)->next;
+	current = (*list)->next;
 
 	while (current != NULL)
 	{
-		listint_t *tmp = current->prev;
+		tmp = current->prev;
 		while (tmp != NULL && tmp->n > current->n)
 		{
 			swap_nodes(list, tmp, current);
--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,14 +8,11 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	size_t x;
-	int *counting_array, max;
-	int i, sum = 0;
-	size_t index = 0;
+	size_t x, index = 0;
+	int *counting_array, max, i, sum = 0;
 
 	if (size <= 1)
 		return;
-
 	max = array[0];
 	for (x = 1; x < size; ++x)
 	{
@@ -24,21 +21,17 @@ void counting_sort(int *array, size_t size)
 			max = array[x];
 		}
 	}
-
 	counting_array = malloc((max + 1) * sizeof(int));
 	if (counting_array == NULL)
 		return;
-
 	for (i = 0; i <= max; ++i)
 	{
 		counting_array[i] = 0;
 	}
-
 	for (x = 0; x < size; ++x)
 	{
 		counting_array[array[x]]++;
 	}
-
 	for (i = 0; i <= max; ++i)
 	{
 		sum += counting_array[i];
@@ -49,13 +42,10 @@ void counting_sort(int *array, size_t size)
 			printf("\n");
 	}
 	for (i = 0; i <= max; ++i)
-	{
 		while (counting_array[i] > 0)
 		{
 			array[index++] = i;
 			counting_array[i]--;
 		}
-	}
-
 	free(counting_array);
 }
